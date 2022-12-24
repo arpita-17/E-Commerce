d55d044fd5c9f517eb13fcec9f4eb0d0c28eae8e
Shubham432000 <shubham7410112997@gmail.com>
2022-12-24 15:28:50 +0530

    css

--- src/Component/CardDetails.jsx
+++ src/Component/CardDetails.jsx
@@ -10,23 +10,24 @@ const CardDetails = () => {
   const cardInfo = useSelector((state) => state.cardReducers.card);
 
   const [price, setPrice] = useState(0);
+
   const dispatch = useDispatch();
 
   const removeCard = (item) => {
     toast.error(`${item.rname} remove from your card`, {
-      position: toast.POSITION.TOP_CENTER
-  });
+      position: toast.POSITION.TOP_CENTER,
+    });
     dispatch(Removecard(item.id));
   };
 
-  const checkOut=()=>{
+  const checkOut = () => {
     swal({
       title: "Your Ordered Placed Succesfully!",
       text: "You clicked the button!",
       icon: "success",
       button: "Ok",
     });
-  }
+  };
   const cardSubmit = (data) => {
     dispatch(Addcard(data));
   };
@@ -37,6 +38,7 @@ const CardDetails = () => {
 
   const total = () => {
     let price = 0;
+
     cardInfo.map((item) => {
       price = price + item.price * item.qnty;
     });
@@ -129,46 +131,12 @@ const CardDetails = () => {
                           >
                             <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                           </svg>
-                          <span className="text-gray-600 ml-3"> {item.rating}</span>
+                          <span className="text-gray-600 ml-3">
+                            {" "}
+                            {item.rating}
+                          </span>
                         </span>
-                        <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                          <a className="text-gray-500">
-                            <svg
-                              fill="currentColor"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              className="w-5 h-5"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                            </svg>
-                          </a>
-                          <a className="text-gray-500">
-                            <svg
-                              fill="currentColor"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              className="w-5 h-5"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                            </svg>
-                          </a>
-                          <a className="text-gray-500">
-                            <svg
-                              fill="currentColor"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth="2"
-                              className="w-5 h-5"
-                              viewBox="0 0 24 24"
-                            >
-                              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                            </svg>
-                          </a>
-                        </span>
+                        <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s"></span>
                       </div>
                       <p className="leading-relaxed">
                         {item.somedata}
@@ -180,14 +148,9 @@ const CardDetails = () => {
                     banjo tattooed umami cardigan. */}
                       </p>
                       <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                        <div className="flex">
-                          <span className="mr-3">Color</span>
-                          <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                          <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                          <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
-                        </div>
+                        
                         <div className="flex ml-6 items-center">
-                          {/* <span className="mr-3">Quantity: */}
+                          
 
                           <div className="custom-number-input h-10 w-32" />
                           <label
@@ -206,7 +169,9 @@ const CardDetails = () => {
                               data-action="decrement"
                               className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-10 rounded-l cursor-pointer outline-none"
                             >
-                              <span className="m-auto text-2xl font-thin">-</span>
+                              <span className="m-auto text-2xl font-thin">
+                                -
+                              </span>
                             </button>
                             {/* <input
                               type="number"
@@ -222,7 +187,9 @@ const CardDetails = () => {
                               data-action="increment"
                               className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-10 rounded-r cursor-pointer"
                             >
-                              <span className="m-auto text-2xl font-thin">+</span>
+                              <span className="m-auto text-2xl font-thin">
+                                +
+                              </span>
                             </button>
                           </div>
 
@@ -255,7 +222,6 @@ const CardDetails = () => {
                           ${item.price}
                         </span>
                         <button
-                        
                           onClick={() => removeCard(item)}
                           className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"
                         >
@@ -280,31 +246,27 @@ const CardDetails = () => {
               </section>
             );
           })}
-          {/* <div className="flex justify-center">
-            <h1>Total:{price}</h1>
-          </div> */}
 
-
-
-
-<div>
-      <div className="mt-4">
-        <div className="py-4 rounded-md shadow">
-          <h3 className="text-xl font-bold text-blue-600">Order Summary</h3>
-          <div className="flex justify-between px-4">
-            <span className="font-bold">Subtotal</span>
-            <span className="font-bold">${price}</span>
-          </div>
-          <div className="flex justify-between px-4">
-            <span className="font-bold">Discount</span>
-            <span className="font-bold text-red-600">- $5.00</span>
-          </div>
-          <div className="flex justify-between px-4">
-            <span className="font-bold">Sales Tax</span>
-            <span className="font-bold">$2.25</span>
-          </div>
-          <div
-            className="
+          <div>
+            <div className="mt-4">
+              <div className="py-4 rounded-md shadow">
+                <h3 className="text-xl font-bold text-blue-600">
+                  Order Summary
+                </h3>
+                <div className="flex justify-between px-4">
+                  <span className="font-bold">Subtotal</span>
+                  <span className="font-bold">${price}</span>
+                </div>
+                <div className="flex justify-between px-4">
+                  <span className="font-bold">Discount</span>
+                  <span className="font-bold text-red-600">- $5.00</span>
+                </div>
+                <div className="flex justify-between px-4">
+                  <span className="font-bold">Sales Tax</span>
+                  <span className="font-bold">$2.25</span>
+                </div>
+                <div
+                  className="
                 flex
                 items-center
                 justify-between
@@ -313,16 +275,16 @@ const CardDetails = () => {
                 mt-3
                 border-t-2
               "
-          >
-            <span className="text-xl font-bold">Total</span>
-            <span className="text-2xl font-bold">{price-5 +2.25}</span>
-          </div>
-        </div>
-      </div>
-      <div className="mt-4">
-        <button
-        onClick={()=>checkOut()}
-          className="
+                >
+                  <span className="text-xl font-bold">Total</span>
+                  <span className="text-2xl font-bold">{price - 5 + 2.25}</span>
+                </div>
+              </div>
+            </div>
+            <div className="mt-4">
+              <button
+                onClick={() => checkOut()}
+                className="
               w-full
               py-2
               text-center text-white
@@ -331,19 +293,21 @@ const CardDetails = () => {
               shadow
               hover:bg-blue-600
             "
-        >
-          Proceed to Checkout
-        </button>
-      </div>
-    </div>
+              >
+                Proceed to Checkout
+              </button>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="mt-10">
-          <h1 className="flex justify-center text-red-500 text-xl font-bold">Your Card is Empty ?  </h1>
+          <h1 className="flex justify-center text-red-500 text-xl font-bold">
+            Your Card is Empty ?{" "}
+          </h1>
           <Empty className="h-80 ml-[200px] mt-8" />
         </div>
       )}
-     <ToastContainer/>
+      <ToastContainer />
     </>
   );
 };
