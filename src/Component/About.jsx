2de633f6a64b5ad546f802aec877b014f7636c25
Shubham432000 <shubham7410112997@gmail.com>
2023-01-04 10:12:30 +0530

    svg-responsive

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -3,7 +3,7 @@ import { ReactComponent as NewYear } from "../image/newyear.svg";
 const About = () => {
   return (
     <>
-    <div className='mt-10 ml-[100px] '>
+    <div className=' w-1/2 sm:w-1/2 md:w-1/2 xl:w-1/3 lg:w-1/3  ml-[50px] md:ml-[200px] lg:ml-[300px] xl:ml-[400px] mt-[150px]'>
     <NewYear/>
     </div>
     </>
